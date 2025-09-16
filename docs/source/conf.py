28accf1d3c5e213c2c29179361c0cffe41b71b0b
YuankangLiu <yuankang.liu@gmail.com>
2025-09-16 16:50:38 +0100

    Update version number to 0.0.18.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ print(">>> sys.path[0]:", sys.path[0])
 project = 'HyLight'
 copyright = '2025, Yuankang Liu, Tom Theuns'
 author = 'Yuankang Liu, Tom Theuns'
-release = '0.0.17'
+release = '0.0.18'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
